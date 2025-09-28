fc6c14116d4490aafc2037c45934002889c6ab12
anshbhatia04 <anshbhatia04@gmail.com>
2025-09-28 22:26:05 +0530

    09: widget layout and session

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,33 +1,19 @@
 "use client";
 
-import { useVapi } from "@/modules/widget/hooks/use-vapi";
-import { Button } from "@workspace/ui/components/button";
+import { use } from "react";
+import { WidgetView } from "@/modules/widget/ui/views/widget-view";
 
-export default function Page() {
+interface Props {
+  searchParams: Promise<{
+    organizationId: string;
+  }>
+};
 
-  const {
-    isSpeaking,
-    isConnecting,
-    isConnected,
-    transcript,
-    startCall,
-    endCall,
-  } = useVapi();
-
+const Page = ({ searchParams }: Props) => {
+  const { organizationId } = use(searchParams);
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh max-w-md mx-auto w-full ">
-        <Button onClick={()=> startCall()}>
-          Start call
-        </Button>
+    <WidgetView organizationId={organizationId}/>
+  );
+};
 
-        <Button onClick={()=> endCall()} variant="destructive">
-          End call
-        </Button>
-
-        <p>isConnected: {`${isConnected}`}</p>
-        <p>isConnecting: {`${isConnecting}`}</p>
-        <p>isSpeaking: {`${isSpeaking}`}</p>
-        <p>{JSON.stringify(transcript, null, 2)}</p>
-    </div>
-  )
-}
+export default Page;
---
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+// import { WidgetFooter } from "../components/widget-footer";
+// import { WidgetHeader } from "../components/widget-header";
+import { WidgetAuthScreen } from "../screens/widget-auth-screen";
+
+interface Props {
+    organizationId: string;
+};
+
+export const WidgetView = ({ organizationId }: Props) => {
+    return (
+        <main className="min-h-screen min-w-screen flex h-full w-full flex-col overflow-hidden rounded-xl border bg-muted">
+            <WidgetAuthScreen />
+            {/* <WidgetFooter /> */}
+        </main>
+    );
+};
