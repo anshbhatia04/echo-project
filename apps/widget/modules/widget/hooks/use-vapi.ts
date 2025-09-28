9de68983622eb2644116c9b92286f91cfe137c9b
anshbhatia04 <anshbhatia04@gmail.com>
2025-09-28 15:21:15 +0530

    06: AI voice assistant

---
+++ apps/widget/modules/widget/hooks/use-vapi.ts
@@ -0,0 +1,83 @@
+import Vapi from "@vapi-ai/web";
+import { useEffect, useState } from "react";
+
+interface TranscriptMessage {
+    role: "user" | "assistant";
+    text: string;
+};
+
+export const useVapi = () => {
+    const [vapi, setVapi] = useState<Vapi | null>(null);
+    const [isConnected, setIsConnected] = useState(false);
+    const [isConnecting, setIsConnecting] = useState(false);
+    const [isSpeaking, setIsSpeaking] = useState(false);
+    const [transcript, setTranscript] = useState<TranscriptMessage[]>([]);
+
+    useEffect(() => {
+        const vapiInstance = new Vapi("15d264ca-04c3-4bba-ab78-a9fbfeb36925");
+        setVapi(vapiInstance);
+
+        vapiInstance.on("call-start", () => {
+            setIsConnected(true);
+            setIsConnecting(false);
+            setTranscript([]);
+        });
+
+        vapiInstance.on("call-end", () => {
+            setIsConnected(false);
+            setIsConnecting(false);
+            setIsSpeaking(false);
+        });
+
+        vapiInstance.on("speech-start", () => {
+            setIsSpeaking(true);
+        });
+
+        vapiInstance.on("speech-end", () => {
+            setIsSpeaking(false);
+        });
+
+        vapiInstance.on("error", (error) => {
+            console.log(error, "VAPI_ERROR");
+            setIsConnecting(false);
+        });
+
+        vapiInstance.on("message", (message) => {
+            if (message.type === "transcript" && message.transcript === "final") {
+                setTranscript((prev) => [
+                    ...prev, 
+                    { 
+                        role: message.role === "user" ? "user" : "assistant", 
+                        text: message.text 
+                    }
+                ]);
+            }
+        });
+
+        return () => {
+            vapiInstance?.stop();
+        }
+    }, []);
+
+    const startCall = () => {
+        setIsConnecting(true);
+
+        if (vapi) {
+            vapi.start("b9271fdc-874d-492a-8f23-215e0512753a");
+        }
+    }
+    const endCall = () => {
+        if (vapi) {
+            vapi.stop();
+        }
+    };
+
+    return {
+        isSpeaking,
+        isConnecting,
+        isConnected,
+        transcript,
+        startCall,
+        endCall,
+    };
+};
